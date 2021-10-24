0457bc92ff04204507a01f47dc0ee3f1121ff8d3
jorgedipra <jorgedipra@gmail.com>
2021-10-24 16:24:51 -0500

    configuración de rutas laravel

--- my-crud/routes/api.php
+++ my-crud/routes/api.php
@@ -3,17 +3,21 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+ 
+ 
+Route::get('categoria', 'CategoriaController@index');
+Route::get('usuarios', 'UsuariosController@index');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'Categoria'], function () {
+    Route::post('add', 'CategoriaController@add');
+    Route::get('edit/{id}', 'CategoriaController@edit');
+    Route::post('update/{id}', 'CategoriaController@update');
+    Route::delete('delete/{id}', 'CategoriaController@delete');
+});
+
+Route::group(['prefix' => 'usuarios'], function () {
+    Route::post('add', 'UsuariosController@add');
+    Route::get('edit/{id}', 'UsuariosController@edit');
+    Route::post('update/{id}', 'UsuariosController@update');
+    Route::delete('delete/{id}', 'UsuariosController@delete');
 });
